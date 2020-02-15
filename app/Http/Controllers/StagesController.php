57c942176c718a9cf832aa92057eb2b9c735ae16
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-15 17:09:58 +0100

    feat:
    make stage two auto pairing

--- app/Http/Controllers/StagesController.php
+++ app/Http/Controllers/StagesController.php
@@ -17,20 +17,26 @@ class StagesController extends Controller
         foreach($checkStage as $check){
 
             $parent = StageTwo::where('level', '<', 2)->get()->first();
-
+            if((count($parent)) > 0){
             $count = StageTwo::where('parent_id', $parent->id)->get();
 
             //if less than 1 set position to be left
             if((count($count)< 1 ) ){
                 $position = 'left';
                 //update level to be 1
+                StageTwo::where('user_id', $parent->user_id)->update(['level'=> 1]);
             }else if((count($count))< 2){
                 $position = 'right';
                 //update users to be cleared
                 User::where('id', $check->id)->update(['stageTwo'=> 'cleared']);
                 //update level to be 2
+                StageTwo::where('user_id', $parent->user_id)->update(['level'=> 2]);
             }
-
+        }else{
+            //do nothing
+            //exit process
+            exit;
+        }
         }
         
         

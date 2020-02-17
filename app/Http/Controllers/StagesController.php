1e73039f9729c57262de4674ca6cf30834a22eaf
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-17 21:19:01 +0100

    chunk:
    final commit for the day

--- app/Http/Controllers/StagesController.php
+++ app/Http/Controllers/StagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\StageFive;
+use App\StageFour;
 use App\StageThree;
 use App\StageTwo;
 use App\User;
@@ -177,4 +179,180 @@ class StagesController extends Controller
   
           
     }
+
+
+    public function stageFour(){
+       
+     
+        /* //set the person to be paired */
+      
+      $parent = User::where('stage', 4)->where('stageFour', 'uncleared')->first();
+      
+      if($parent){
+      //check if the  first person to enter stage 2 has been paired
+      $checkStage = User::where('stage', 4)->where('stageFour', 'uncleared')->where('id', '!=', $parent->id )->get();
+      
+      if ($checkStage){
+      //count where the user_id appears as parent Id
+      foreach($checkStage as $check){
+
+          /*$parent = StageTwo::where('parent_id', $parent->id)->where('level', '<', 2)->get()->first();
+          if((count($parent)) > 0){*/
+
+          $count = StageFour::where('parent_id', $parent->id)->get();
+          $root_id = $count->parent_id;
+
+          //if less than 1 set position to be left
+          if((count($count)< 1 ) ){
+
+
+              $position = 'left';
+
+              //insert into stageTwo table
+
+              $pair = new StageFour();
+              $pair->user_id = $check->id;
+              $pair->referral = 'NA';
+              $pair->parent_id = $parent->id;
+              $pair->root_id = $root_id;
+              $pair->position = $position;
+              $pair->stage = 4;
+              $pair->level = 1;
+              $pair->save();     
+           
+
+
+          }else if((count($count))< 2){
+
+
+              $position = 'right';
+              //update level to be 2
+              
+              $pair = new StageFour();
+              $pair->user_id = $check->id;
+              $pair->referral = 'NA';
+              $pair->parent_id = $parent->id;
+              $pair->root_id = $root_id;
+              $pair->position = $position;
+              $pair->stage = 4;
+              $pair->level = 2;
+              $pair->save();  
+
+               //update users to be cleared
+               User::where('id', $parent->id)->update(['stageFour'=> 'cleared']);
+
+          }else{
+              //look for the next user
+              $parent = User::where('stage', 4)->where('stageFour', 'uncleared')->first();
+          }
+      /*}else{
+          //do nothing
+          //exit process
+          exit;
+      }*/
+      }
+  }else{
+      echo "Nobodody to pair $parent->username with";
+      exit;
+  }
+      
+  }else{
+      echo "Nothing to pair";
+      die;
+  }
+      //parent of your parent is your root
+
+      
+}
+
+
+
+
+public function stageFive(){
+       
+     
+    /* //set the person to be paired */
+  
+  $parent = User::where('stage', 5)->where('stageFive', 'uncleared')->first();
+  
+  if($parent){
+  //check if the  first person to enter stage 2 has been paired
+  $checkStage = User::where('stage', 5)->where('stageFive', 'uncleared')->where('id', '!=', $parent->id )->get();
+  
+  if ($checkStage){
+  //count where the user_id appears as parent Id
+  foreach($checkStage as $check){
+
+      /*$parent = StageTwo::where('parent_id', $parent->id)->where('level', '<', 2)->get()->first();
+      if((count($parent)) > 0){*/
+
+      $count = StageFive::where('parent_id', $parent->id)->get();
+      $root_id = $count->parent_id;
+
+      //if less than 1 set position to be left
+      if((count($count)< 1 ) ){
+
+
+          $position = 'left';
+
+          //insert into stageTwo table
+
+          $pair = new StageFive();
+          $pair->user_id = $check->id;
+          $pair->referral = 'NA';
+          $pair->parent_id = $parent->id;
+          $pair->root_id = $root_id;
+          $pair->position = $position;
+          $pair->stage = 5;
+          $pair->level = 1;
+          $pair->save();     
+       
+
+
+      }else if((count($count))< 2){
+
+
+          $position = 'right';
+          //update level to be 2
+          
+          $pair = new StageFive();
+          $pair->user_id = $check->id;
+          $pair->referral = 'NA';
+          $pair->parent_id = $parent->id;
+          $pair->root_id = $root_id;
+          $pair->position = $position;
+          $pair->stage = 5;
+          $pair->level = 2;
+          $pair->save();  
+
+           //update users to be cleared
+           User::where('id', $parent->id)->update(['stageFive'=> 'cleared']);
+
+      }else{
+          //look for the next user
+          $parent = User::where('stage', 5)->where('stageFive', 'uncleared')->first();
+      }
+  /*}else{
+      //do nothing
+      //exit process
+      exit;
+  }*/
+  }
+}else{
+  echo "Nobodody to pair $parent->username with";
+  exit;
+}
+  
+}else{
+  echo "Nothing to pair";
+  die;
+}
+  //parent of your parent is your root
+
+  
+}
+
+
+
+
 }

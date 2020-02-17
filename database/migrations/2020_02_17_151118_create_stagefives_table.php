4b50f34e5262eb28462b448239678b6f098b7bbb
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-17 15:13:03 +0100

    chunk:
    make migration tables

---
+++ database/migrations/2020_02_17_151118_create_stagefives_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStagefivesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stagefives', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id',false,11);
+            $table->string('referral');
+            $table->string('parent_id');
+            $table->string('root_id');
+            $table->enum('position',['left', 'right','float']);
+            $table->string('stage');
+            $table->string('level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stagefives');
+    }
+}

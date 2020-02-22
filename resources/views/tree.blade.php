10d60110ede00060239ab7d968368dd465b9c2a6
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-22 11:06:06 +0100

    Style:
    restyle tree

--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -12,27 +12,27 @@
             
         </nav>
         <main>
-        <ul>
-                <li>Parent <b>{{$parent}}</b></li>
-          <ul>
-              <li> <b>Me </b>
-                  <ul style="display: flex">
-                      <li >child1: <b>{{$child1}}</b>
-                          <ul style="display: flex">
-                              <li>child1 of child1: <b>{{$child3}}</b></li>
-                              <li style="margin-left: 2rem">child2 of child1: <b>{{$child4}}</b></li>
-                            </ul>
-                        </li>
-                      <li style="margin-left: 2rem">child2: <b>{{$child_2}}</b>
-                          <ul style="display: flex">
-                              <li>child1 of child2: <b>{{$child5}}</b></li>
-                              <li style="margin-left: 2rem"> child2 of child2: <b>{{$child6}}</b> </li>
-                          </ul>
-                      </li>
-                  </ul>
-                </li>
-            </ul>
-        </ul>
+        <!-- using Div for the tree-->
+        <div id="trees">
+            <div id="parent">
+                <p style="text-align: center">My Parent <b>{{$parent}}</b></p>
+                <p style="margin-left: 9rem"> <b>Me </b>
+                        <div id="children" style="display: flex">
+                            <div id="child1"><b>1: {{$child1}} </b> 
+                                <div id="childs" style="display: flex; "> <p id="child3" style=" margin: 1rem; margin-left:0;"><b>3: {{$child3}}</b></p>
+                                    <p id="child4" style="margin-right:1rem;"><b> 4: {{$child4}}</b></p>
+                                </div>
+                            </div>
+                            <div id="child2"><b> 2: {{$child_2}}</b>
+                                <div id="childs" style="display: flex;"> <p id="child5" style=" margin: 1rem; margin-left:0;"><b>5: {{$child5}}</b></p>
+                                        <p id="child6"><b>6: {{$child6}}</b></p>
+                                </div>
+                            </div>
+                        </div>
+                </p>
+            </div>
+        </div>
+       
         </main>
     </div>
 </div>

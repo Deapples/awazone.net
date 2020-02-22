df8719d2062119fed43f96a1a7db4554b17feb8c
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-22 18:08:31 +0100

    style:
    make geneology responsive

--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -38,6 +38,75 @@ margin-right: 18rem;
     position: fixed;
     bottom: 26.5rem;
 }
+#me, #myparent{
+    margin-left: 12rem;
+}
+@media (max-width: 600px){
+    body{
+        margin: 0;
+        overflow: scroll;
+    }
+    main{
+        margin: 0;
+        width: 50%;
+        font-size:12px;
+        overflow: scroll;
+    }
+    hr { 
+        width: 12rem;
+margin-left: 1.66rem;
+margin-right: 18rem;
+}
+#me, #myparent{
+    margin-left: 7rem;
+}
+svg{
+    margin-left: -5rem;
+}
+#child2{
+   position: absolute;
+   left: 10rem;
+}
+#child4{
+    margin-right:0;
+}
+#trees{
+    position: absolute;
+    left: 1rem;
+    width:40%;
+    
+}
+#childhrleft{
+    width: 1.0rem;
+}
+#childhrright{
+   margin-left: 7rem;
+    top: 15.3rem;
+}
+#left{
+     width: 1.1rem;
+}
+#right{
+    margin-left: 13rem;
+   
+    top: 13rem;
+   
+}
+nav{
+    margin: 0;
+    margin-top: 1rem;
+    float: left;
+}
+html, body {
+    position:fixed;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+    overflow: hidden;
+    
+}
+}
 </style>
 <div id="body">
     @include('aside')
@@ -50,11 +119,11 @@ margin-right: 18rem;
         <!-- using Div for the tree-->
         <div id="trees">
             <div id="parent">
-                <p style="margin-left: 10rem">My Parent: <b>{{$parent}}</b></p>
+                <p id="myparent">My Parent: <b>{{$parent}}</b></p>
                 <svg height="20" width="500">
   <line x1="199" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
 </svg>
-                <p style="margin-left: 12rem"> <b>Me </b></p>
+                <p  id="me"> <b>Me </b></p>
                 <svg height="20" width="500">
                     <line x1="199" y1="0" x2="200" y2="200" style="stroke:rgb(255,0,0);stroke-width:2" />
                 </svg><hr /> <hr id="left" /> <hr id="right" />
@@ -65,7 +134,7 @@ margin-right: 18rem;
                                     <hr id="childhrright" />
 
 
-                                <div id="childs" style="display: flex; "> <p id="child3" style=" margin: 1rem; margin-left:0;"><b> {{$child3}}</b></p>
+                                <div id="children" style="display: flex; "> <p id="child3" style=" margin: 1rem; margin-left:0;"><b> {{$child3}}</b></p>
                                     <p id="child4" style="margin-right:1rem;"><b>  {{$child4}}</b></p>
                                 </div>
                             </div>
@@ -76,7 +145,7 @@ margin-right: 18rem;
                             <hr id="childhr" />
                                     <hr id="childhrleft" />
                                     <hr id="childhrright" />
-                                <div id="childs" style="display: flex;"> <p id="child5" style=" margin: 1rem; margin-left:0;"><b> {{$child5}}</b></p>
+                                <div id="children" style="display: flex;"> <p id="child5" style=" margin: 1rem; margin-left:0;"><b> {{$child5}}</b></p>
                                         <p id="child6"><b> {{$child6}}</b></p>
                                 </div>
                             </div>

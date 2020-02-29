fdb7aebf171e1a9e0a72c1a1180f6a28163ad88b
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-29 16:59:47 +0100

    feat
    make authorization for withdrawal

--- app/Http/Controllers/withdrawEarningsController.php
+++ app/Http/Controllers/withdrawEarningsController.php
@@ -26,6 +26,7 @@ class withdrawEarningsController extends Controller
         }
     }
 
+    
 
     /**
      * @param request => email, password, amount
@@ -33,39 +34,68 @@ class withdrawEarningsController extends Controller
      */
 
     public function withdrawFunds(Request $request){
-
+        $id = session('id');
         $p_email = $request->p_email;
         $p_password = $request->p_password;
         $amount = $request->amount * 350;
 
-        //make api call
-        //make api call here
-        $dat = [
-            'p_email' => $p_email,
-            'amount' => $amount,
-            'p_password' => $p_password
-        ];              
-        $url = 'http://api.printmoney.com/withdraw';
 
-        $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $dat);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $makePayment = curl_exec($curl);
+        $check_balance = User::where('id', $id)->get();
+
+        if(count($check_balance)> 0){
+            if(($check_balance[0]->balance) >= ($amount/350)){
+                 //make api call
+                //make api call here
+                $dat = [
+                    'p_email' => $p_email,
+                    'amount' => $amount,
+                    'p_password' => $p_password
+                ];              
+                $url = 'http://api.printmoney.com/withdraw';
         
-        $payment = json_decode($makePayment);
+                $curl = curl_init($url);
+                    curl_setopt($curl, CURLOPT_POST, true);
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $dat);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                $makePayment = curl_exec($curl);
+                
+                $payment = json_decode($makePayment);
+        
+                if( $payment->status == 'ok'){
+                    //enter transaction history here
+                   
+                    $trans = new Transaction();
+                    $trans->user_id = $id;
+                    $trans->description = $amount . "Withdrawal into printmoney";
+                    $trans->amount = $amount;
+                    $input = $trans->save();
+        
+                    if($input){
+                        echo "<script>alert('Payment Successfully made into your printmoney Account'); window.location=('/withdraw');</script>";
+                    }else{
+                        echo "<script>alert('An error occur kindly contact customer care'); window.location=('/withdraw');</script>";
+                    }
+        
+        
+                   
+                }else{
+                    echo "<script>alert('Payment  cannot be made at the moment'); window.location=('/withdraw');</script>";
+        
+                }
+        
 
-        if( $payment->status == 'ok'){
-            //enter transaction history here
-            $id = session('id');
-            $trans = new Transaction();
-            $trans->user_id = $id;
-            $trans->description = $amount . "Withdrawal into printmoney";
-            echo "<script>alert('Payment Successfully made into your printmoney Account'); window.location=('/withdraw');</script>";
+            }else{
+                //insufficient fund
+                echo "<script>alert('Insufficient Fund'); window.location=('/withdraw');</script>";
+            }
+            
         }else{
-            echo "<script>alert('Payment Payment cannot be made at the moment'); window.location=('/withdraw');</script>";
-
+            //not a user
+            echo "<script>alert('Invalid User'); window.location=('/withdraw');</script>";
         }
-
+       
+     
+       
+       
     }
 }
